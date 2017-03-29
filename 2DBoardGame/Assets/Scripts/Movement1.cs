41ca66f96b6e180eb0a368a250e31ec98e190648
BrandonTClarke <brandontclarke@gmail.com>
2017-03-28 17:39:04 -0700

    FIX and new start
    I dont even know....

---
+++ 2DBoardGame/Assets/Scripts/Movement1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+class Movement1 : MonoBehaviour
+{
+
+	Vector3 pos;                                // For movement
+	float speed = 3.0f;                         // Speed of movement
+
+	void Start () {
+		pos = transform.position;          // Take the initial position
+	}
+
+	void FixedUpdate () {
+		if(Input.GetAxisRaw("Horizontal") < 0 && transform.position == pos) {        // Left
+			pos += Vector3.left;
+		}
+		if(Input.GetAxisRaw("Horizontal") > 0 && transform.position == pos) {        // Right
+			pos += Vector3.right;
+		}
+		if(Input.GetAxisRaw("Vertical") > 0 && transform.position == pos) {        // Up
+			pos += Vector3.up;
+		}
+		if(Input.GetAxisRaw("Vertical") < 0 && transform.position == pos) {        // Down
+			pos += Vector3.down;
+		}
+		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
+	}
+
+	/* public float speed = 5.0f; // player speed
+	public Rigidbody2D rb2D;
+	private Vector3 pos; // position
+
+	void Start()
+	{
+		rb2D = GetComponent<Rigidbody2D>();
+		pos = transform.position; // Take the initial position
+	}
+
+	void FixedUpdate()
+	{
+	Move();
+	}
+
+	void Move()
+		{
+		// If key pressed && not moving
+		if (Input.GetAxisRaw("Horizontal") < 0) // Left
+			rb2D.MovePosition(rb2D.position + Vector2.left * Time.fixedDeltaTime);
+		if (Input.GetAxisRaw("Horizontal") > 0) // Right
+			rb2D.MovePosition(rb2D.position + Vector2.right * Time.fixedDeltaTime);
+		if (Input.GetAxisRaw("Vertical") > 0) // Up
+			rb2D.MovePosition(rb2D.position + Vector2.up * Time.fixedDeltaTime);
+		if (Input.GetAxisRaw("Vertical") < 0) // Down
+			rb2D.MovePosition(rb2D.position + Vector2.down * Time.fixedDeltaTime);
+
+	}
+
+*/
+}
