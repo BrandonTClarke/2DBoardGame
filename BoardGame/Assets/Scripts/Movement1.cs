3a354616416ca9e9827822ea3c6cda740dec180f
BrandonTClarke <brandontclarke@gmail.com>
2017-04-19 16:13:47 -0700

    Made Movement Limits Based on dice
    You now have to hit end turn to move again, sweet!

--- BoardGame/Assets/Scripts/Movement1.cs
+++ BoardGame/Assets/Scripts/Movement1.cs
@@ -7,6 +7,7 @@ class Movement1 : MonoBehaviour
 	Vector3 pos;                                // For movement
 	float speed = 2.0f; 					 // Speed of movement
 	int moveNum = StatHolder.Dice;
+	int movesMade = 0;
 
 	void Start () {
 		pos = transform.position; // Take the initial position
@@ -14,30 +15,32 @@ class Movement1 : MonoBehaviour
 	}
 
 	void FixedUpdate () {
-		for (int i = 0; i < moveNum; i++){
-
-			if(Input.GetAxisRaw("Horizontal") < 0 && transform.position == pos) {        // Left
-				pos += Vector3.left;
-				Debug.Log(i);
-			}
-			if(Input.GetAxisRaw("Horizontal") > 0 && transform.position == pos) {        // Right
-				pos += Vector3.right;
-				Debug.Log(i);
-			}
-			if(Input.GetAxisRaw("Vertical") > 0 && transform.position == pos) {        // Up
-				pos += Vector3.up;
-				Debug.Log(i);
-			}
-			if(Input.GetAxisRaw("Vertical") < 0 && transform.position == pos) {        // Down
-				pos += Vector3.down;
-				Debug.Log(i);
-			}
-			transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
+		if(Input.GetAxisRaw("Horizontal") < 0 && transform.position == pos && movesMade < moveNum ) {        // Left
+			pos += Vector3.left;
+			movesMade++;
+			Debug.Log(movesMade);
 		}
-}
+		if(Input.GetAxisRaw("Horizontal") > 0 && transform.position == pos && movesMade < moveNum ) {        // Right
+			pos += Vector3.right;
+			movesMade++;
+			Debug.Log(movesMade);
+		}
+		if(Input.GetAxisRaw("Vertical") > 0 && transform.position == pos && movesMade < moveNum ) {        // Up
+			pos += Vector3.up;
+			movesMade++;
+			Debug.Log(movesMade);
+		}
+		if(Input.GetAxisRaw("Vertical") < 0 && transform.position == pos && movesMade < moveNum ) {        // Down
+			pos += Vector3.down;
+			movesMade++;
+			Debug.Log(movesMade);
+		}
+			transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);  
+	}
 
 	public void ResetDice(){
-		moveNum = StatHolder.Dice;
+		movesMade = 0;
+		Debug.Log (moveNum);
 	}
 
 	/* public float speed = 5.0f; // player speed
