20ed4b5634515bff8e6b2b9fe7229a4ffe459349
SimonDaniel Altamirano <messi10gol@live.com>
2017-05-08 21:01:48 -0700

    Added more things
    Will finish battle scene and camera tweeking tommorow

---
+++ BoardGame/Assets/Scripts/Battle.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour {
+	/*
+	public enum BattleStates {
+		Start,
+		PlayerChoice,
+		PlayerAnimate,
+		EnemyChoice,
+		Lose, 
+		Win
+	}
+
+	private BattleStates currentState;
+
+
+	void Start () {
+		currentState = BattleStates.Start;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (currentState);
+		switch (currentState) {
+		case (BattleStates.Start):
+			//setup battle function
+			break;
+		case (BattleStates.PlayerChoice):
+			break;
+		case (BattleStates.EnemyChoice):
+			break;
+		case (BattleStates.Lose):
+			break;
+		case (BattleStates.Win):
+			break;
+
+		}
+	}
+
+	void OnGUI(){
+		if(GUILayout.Button("Next State")){
+			if (currentState == BattleStates.Start) {
+				currentState = BattleStates.PlayerChoice;
+			} else if (currentState == BattleStates.PlayerChoice) {
+				currentState == BattleStates.EnemyChoice;
+			} else if (currentState == BattleStates.EnemyChoice) {
+				currentState == BattleStates.Lose;
+			} else if (currentState == BattleStates.Lose) {
+				currentState == BattleStates.Win;
+			} else if (currentState == BattleStates.Win) {
+				currentState == BattleStates.Start;
+		}
+	  }
+	}*/
+}
